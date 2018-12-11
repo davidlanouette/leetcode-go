f22f873dcb5dc71f43abfa70b85b7d0263d4960e
David Lanouette <dlanouet@cisco.com>
2018-12-11 08:13:22 -0500

    ReverseString 3 - fixed multi-byte string issue

--- reverseWords.go
+++ reverseWords.go
@@ -14,8 +14,9 @@ func reverseWords(s string) string {
 
 	numSpaces := strings.Count(s, " ")
 	for n, word := range words {
-		for i := len(word) - 1; i >= 0; i-- {
-			buf.WriteString(string(word[i]))
+		runes := []rune(word)
+		for i := len(runes) - 1; i >= 0; i-- {
+			buf.WriteString(string(runes[i]))
 		}
 		if n < numSpaces {
 			buf.WriteString(" ")
