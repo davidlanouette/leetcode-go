18ad653e7536bc98848adad34d80f4975e9b346e
David Lanouette <dlanouette@MovableInk.com>
2019-08-23 09:05:07 -0400

    Update twoSums to use a map lookup
    Instead of using an inner loop, look up the difference in a map to see
    if the needed value to complete the pair exists.

--- twoSum.go
+++ twoSum.go
@@ -1,6 +1,19 @@
 package leetcode
 
 func twoSum(nums []int, target int) []int {
-
-	return []int{0}
+	// fmt.Println("nums =", nums, ", target =", target)
+	numMap := make(map[int]int)
+	for n, v := range nums {
+		numMap[v] = n
+	}
+	// fmt.Println("numMap =", numMap)
+	for i := 0; i < len(nums); i++ {
+		j, ok := numMap[target-nums[i]]
+		// fmt.Println("i =", i, ", nums[i] =", nums[i], ", target-nums[i] =", target-nums[i], ", j =", j, ", ok =", ok)
+		if ok && i != j {
+			return []int{i, j}
+		}
+	}
+	// couldn't find a match.  return empty slice
+	return []int{}
 }
--- twoSum_test.go
+++ twoSum_test.go
@@ -1,17 +1,49 @@
 package leetcode
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTwoSum(t *testing.T) {
-	//Given nums = [2, 7, 11, 15], target = 9,
-	//	Because nums[0] + nums[1] = 2 + 7 = 9,
-	// return [0, 1].
-	input := []int{2, 7, 11, 15}
-	result := twoSum(input, 9)
-	assert.Equal(t, 2, len(result))
 
+	testVals := []struct {
+		input    []int
+		target   int
+		expected []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{2, 7, 11, 15}, 13, []int{0, 2}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+	}
+
+	for _, v := range testVals {
+		result := twoSum(v.input, v.target)
+		assert.Equal(t, 2, len(v.expected))
+		assert.Equal(t, v.expected[0], result[0])
+		assert.Equal(t, v.expected[1], result[1])
+	}
+}
+
+func BenchmarkTwoSum(b *testing.B) {
+	testVals := []struct {
+		input    []int
+		target   int
+		expected []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{2, 7, 11, 15}, 13, []int{0, 2}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+	}
+
+	var result []int
+
+	for _, v := range testVals {
+		for i := 0; i < b.N; i++ {
+			result = twoSum(v.input, v.target)
+		}
+	}
+	fmt.Print(result)
 }
