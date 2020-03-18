3c2ea4e8e14ff3ea5cca297ee7a7a0db8e93ed42
David Lanouette <dlanouette@MovableInk.com>
2020-03-18 13:51:44 -0400

    Move logic from it's own file into test

--- addTwoNumbers_test.go
+++ addTwoNumbers_test.go
@@ -2,10 +2,101 @@ package leetcode
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+	Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+	Output: 7 -> 0 -> 8
+	Explanation: 342 + 465 = 807.
+*/
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	var head, prev, node *ListNode
+	var num1, num2, leftOver int
+
+	for (l1 != nil) || (l2 != nil) {
+		if l1 != nil {
+			num1 = l1.Val
+		} else {
+			num1 = 0
+		}
+		if l2 != nil {
+			num2 = l2.Val
+		} else {
+			num2 = 0
+		}
+
+		sum := num1 + num2 + leftOver
+
+		leftOver = sum / 10
+
+		node = &ListNode{sum % 10, nil}
+
+		if prev == nil {
+			head = node
+		} else {
+			prev.Next = node
+		}
+		prev = node
+
+		if l1 != nil {
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			l2 = l2.Next
+		}
+	}
+
+	if leftOver != 0 {
+		node.Next = &ListNode{leftOver, nil}
+	}
+
+	return head
+}
+
+// ListNode is a node in a singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// Print converts the linked list into a string
+func (ln *ListNode) Print() string {
+	out := ""
+
+	for ln != nil {
+		out = out + strconv.Itoa(ln.Val)
+		ln = ln.Next
+	}
+
+	return out
+}
+
+func sliceToList(nums []int) *ListNode {
+
+	var head, prev *ListNode
+
+	for _, i := range nums {
+		node := &ListNode{i, nil}
+		if prev == nil {
+			head = node
+		} else {
+			prev.Next = node
+		}
+		prev = node
+	}
+	return head
+}
 
 func TestAddTwoNumbers(t *testing.T) {
 	assert.True(t, true)
@@ -46,20 +137,4 @@ func TestAddTwoNumbers(t *testing.T) {
 			num1.Print(), num2.Print(), expected.Print(), actual.Print())
 		assert.Equal(t, expected.Print(), actual.Print())
 	}
-}
-
-func sliceToList(nums []int) *ListNode {
-
-	var head, prev *ListNode
-
-	for _, i := range nums {
-		node := &ListNode{i, nil}
-		if prev == nil {
-			head = node
-		} else {
-			prev.Next = node
-		}
-		prev = node
-	}
-	return head
 }
--- twoSum_test.go
+++ twoSum_test.go
@@ -7,6 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+	Given nums = [2, 7, 11, 15], target = 9,
+
+	Because nums[0] + nums[1] = 2 + 7 = 9,
+	return [0, 1].
+
+*/
+func twoSum(nums []int, target int) []int {
+	// fmt.Println("nums =", nums, ", target =", target)
+	numMap := make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		// fmt.Println("numMap =", numMap)
+		j, ok := numMap[target-nums[i]]
+		// fmt.Println("i =", i, ", nums[i] =", nums[i], ", target-nums[i] =", target-nums[i], ", j =", j, ", ok =", ok)
+		if ok && i != j {
+			return []int{j, i}
+		}
+		numMap[nums[i]] = i
+	}
+	// couldn't find a match.  return empty slice
+	return []int{}
+}
+
 func TestTwoSum(t *testing.T) {
 
 	testVals := []struct {
